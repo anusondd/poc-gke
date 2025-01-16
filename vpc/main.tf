34c22628db516686083ff09f041d086657c2ef74
Anuson <anusondd@gmail.com>
2025-01-17 01:00:22 +0700

    NAT

--- vpc/main.tf
+++ vpc/main.tf
@@ -37,3 +37,22 @@ resource "google_compute_subnetwork" "this" {
   network                  = google_compute_network.this.id
   private_ip_google_access = true
 }
+
+# NAT ROUTER
+resource "google_compute_router" "this" {
+  name    = "${var.name}-${local.type[1]}-router"
+  region  = google_compute_subnetwork.this[1].region
+  network = google_compute_network.this.id
+}
+
+resource "google_compute_router_nat" "this" {
+  name                               = "${var.name}-${local.type[1]}-router-nat"
+  router                             = google_compute_router.this.name
+  region                             = google_compute_router.this.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+  subnetwork {
+    name                    = "${var.name}-${local.type[1]}-subnetwork"
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+}
